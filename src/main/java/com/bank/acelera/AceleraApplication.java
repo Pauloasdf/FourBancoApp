eb24599de68c4f235272632130cca108d81bbe7f
laurocj <laurocj@gmail.com>
2020-02-25 23:28:05 -0300

    Primeiro commit

---
+++ src/main/java/com/bank/acelera/AceleraApplication.java
@@ -0,0 +1,13 @@
+package com.bank.acelera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceleraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/acelera/AceleraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.acelera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceleraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
