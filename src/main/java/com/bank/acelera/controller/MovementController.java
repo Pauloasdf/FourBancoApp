fc143f73f7a86d548e6d8b5bb881e8c47efd5498
laurocj <laurocj@gmail.com>
2020-03-08 16:08:39 -0300

    Teste para o controller

--- src/main/java/com/bank/acelera/controller/MovementController.java
+++ src/main/java/com/bank/acelera/controller/MovementController.java
@@ -2,32 +2,26 @@ package com.bank.acelera.controller;
 
 import com.bank.acelera.controller.request.MovementRequest;
 import com.bank.acelera.controller.response.MovementResponse;
-import com.bank.acelera.model.Account;
-import com.bank.acelera.repository.AccountRepository;
 import com.bank.acelera.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/transaction")
 public class MovementController {
 
+    @Autowired
     private AccountService accountService;
-
-    private AccountRepository accountRepository;
-
-    public MovementController(AccountService accountService) {
-        this.accountService = accountService;
-    }
 
     @PostMapping
     public ResponseEntity createNewMovement(@RequestBody MovementRequest movementRequest){
 
         if(accountService.addMovement(movementRequest)){
-            return ResponseEntity.status(HttpStatus.OK).body("Realizada com sucesso");
+            return ResponseEntity.status(HttpStatus.CREATED).body("Realizada com sucesso");
         }
 
         return ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
--- src/test/java/com/bank/acelera/MovementControllerTest.java
+++ src/test/java/com/bank/acelera/MovementControllerTest.java
@@ -7,45 +7,82 @@ package com.bank.acelera;
 
 import com.bank.acelera.service.AccountService;
 import com.bank.acelera.controller.MovementController;
-import com.bank.acelera.model.Account;
-import com.bank.acelera.model.Physical;
-import org.assertj.core.api.Assertions;
+import com.bank.acelera.controller.request.MovementRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
 import org.hamcrest.Matchers;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 /**
  *
  * @author lauro
  */
-@WebMvcTest(MovementController.class)
+//@WebMvcTest(MovementController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MovementControllerTest {
     
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @Autowired
     AccountService accountService;
     
+    // Used for converting heroes to/from JSON
+    private ObjectMapper mapper = new ObjectMapper();
+    
     @Test
-    @Sql({"/data.sql"})
     void apiGetMovements() throws Exception {
         mvc.perform(get("/transaction/22222223")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$", Matchers.hasSize(11)))
-                .andExpect(jsonPath("$[0].value", Matchers.is("10.0")));
+                .andExpect(jsonPath("$[0].value", Matchers.is(10.0)));
     }
 
+    @Test
+    void apiCreateNewMovement() throws Exception {
+
+        MovementRequest movementRequest = new MovementRequest();
+        movementRequest.setAccountNumber(22222223L);
+        movementRequest.setType(2);
+        movementRequest.setValue(10.0F);
+        
+        byte[] movementJson = toJson(movementRequest);
+
+        mvc.perform(
+                post("/transaction")
+                .content(movementJson)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+    }
+    
+    /**
+     * Convert object to JSON bytes.
+     * 
+     * @param object
+     *            The object to JSONify
+     * @return byte array with JSON representation
+     * @throws Exception
+     */
+    private byte[] toJson(Object object) throws Exception {
+        return this.mapper.writeValueAsString(object).getBytes();
+    }
 
 }
